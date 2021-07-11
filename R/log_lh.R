72632babaab6d7c32f68eb09b02f7f3b8fbebb39
Philipp Külpmann <philipp@kuelpmann.org>
2021-07-11 17:43:10 +0200

    oxygen initialization

--- R/log_lh.R
+++ R/log_lh.R
@@ -1,6 +1,15 @@
+#' Calculate log-likelihood for a theory
+#'
+#' @param model_prediction Model prediction for a theory (matrix of doubles)
+#' @param data_experiment Data of the experiment (matrix of ints)
+#'
+#' @return The log-likelihood of the theory
+#'
+#' @examples
 log_lh <- function(model_prediction, data_experiment) {
   # data_experiment: 2x2, integers
   # model_prediction: 2x2, double, rowSum = 1, single model!
+
   # input consistency checks
   # input -> output
   output <- data_experiment * log(model_prediction)+(rowSums(data_experiment)-data_experiment)*log(1-model_prediction)
