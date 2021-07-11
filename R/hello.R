35935ddb37f5090ba895f0d7c40b75df0aec183d
Philipp Külpmann <philipp@kuelpmann.org>
2021-07-11 12:41:28 +0200

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,19 @@
+# Useful packages:
+# library(roxygen2) # In-Line Documentation for R
+# library(devtools) # Tools to Make Developing R Packages Easier
+# library(testthat) # Unit Testing for R
+# library(usethis)  # Automate Package and Project Setup
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
