2d2126c62232f44bbb10f5aa95280c3c61812c35
PhilippKuelpmann <philipp.kuelpmann@gmail.com>
2021-11-20 18:02:12 +0100

    fixed mistakes in both test cases

--- tests/testthat/test_vuong.R
+++ tests/testthat/test_vuong.R
@@ -1,6 +1,6 @@
 context("test vuong")
 
-experimental_data <- matrix(10, nrow = 1, ncol = 2, byrow=TRUE)
+experimental_data <- matrix(10, nrow = 5, ncol = 2, byrow=TRUE)
 theory_1 <- matrix(c(.5, .5), nrow = 5, ncol = 2, byrow=TRUE)
 theory_2 <- matrix(c(.9, .1, .1,.9), nrow = 5, ncol = 2, byrow=TRUE)
 
@@ -10,12 +10,12 @@ expect_that(
 
 
 result_a <- vuong_statistic(
-  data = matrix(data = 10, nrow = 1, ncol = 2, byrow=TRUE),
+  data = matrix(data = 1, nrow = 1, ncol = 2, byrow=TRUE),
   pred_I = matrix(data = c(.5, .5), nrow = 1, ncol = 2, byrow=TRUE),
-  pred_J = matrix(data = c(.9, .1), nrow = 1, ncol = 2, byrow=TRUE),
+  pred_J = matrix(data = c(.9, .1), nrow = 1, ncol = 2, byrow=TRUE)
 )
 
-expec_that(result_a,
+expect_that(result_a,
            equals((log(.5/.9) + log(0.5/0.1))/(1/2 * (log(0.5/0.9) -
                                               log(0.5/0.1))^2)^(0.5)))
 
