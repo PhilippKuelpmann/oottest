475ae7706b4bdc1e6d8f6ac1731c32f567022533
PhilippKuelpmann <philipp.kuelpmann@gmail.com>
2021-11-20 16:48:39 +0100

    Added test data: simple_test_data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Simple Test Data
+#'
+#' Mostly to test data im/exporting
+#'
+#' @format A matrix with 3 rows and 2 columns
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"simple_test_data"
