88a76f775b0723a168e0411a32a835395275dbf6
PhilippKuelpmann <philipp.kuelpmann@gmail.com>
2021-11-20 19:16:22 +0100

    replaced depreciated test commands (expect_that and context)

--- tests/testthat/test_vuong.R
+++ tests/testthat/test_vuong.R
@@ -1,12 +1,10 @@
-context("test vuong")
+experimental_data <- matrix(10, nrow = 8, ncol = 2, byrow=TRUE)
+theory_1 <- matrix(c(.5, .5), nrow = 8, ncol = 2, byrow=TRUE)
+theory_2 <- matrix(c(.9, .1, .1,.9), nrow = 8, ncol = 2, byrow=TRUE)
 
-experimental_data <- matrix(10, nrow = 5, ncol = 2, byrow=TRUE)
-theory_1 <- matrix(c(.5, .5), nrow = 5, ncol = 2, byrow=TRUE)
-theory_2 <- matrix(c(.9, .1, .1,.9), nrow = 5, ncol = 2, byrow=TRUE)
-
-expect_that(
+expect_equal(
   vuong_statistic(experimental_data, theory_1, theory_2),
-  equals(- vuong_statistic(experimental_data, theory_2, theory_1)))
+  - vuong_statistic(experimental_data, theory_2, theory_1))
 
 
 result_a <- vuong_statistic(
@@ -15,7 +13,7 @@ result_a <- vuong_statistic(
   pred_J = matrix(data = c(.9, .1), nrow = 1, ncol = 2, byrow=TRUE)
 )
 
-expect_that(result_a,
-           equals((log(.5/.9) + log(0.5/0.1))/(1/2 * (log(0.5/0.9) -
-                                              log(0.5/0.1))^2)^(0.5)))
+expect_equal(result_a,
+           (log(.5/.9) + log(0.5/0.1))/(1/2 * (log(0.5/0.9) -
+                                              log(0.5/0.1))^2)^(0.5))
 
