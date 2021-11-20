03cfa6c5608e896db6efd0772f63fe30776e5dd2
PhilippKuelpmann <philipp.kuelpmann@gmail.com>
2021-11-20 18:12:20 +0100

    added structure for data documentation

--- R/data.R
+++ R/data.R
@@ -10,3 +10,42 @@
 #' }
 #' @source \url{http://madeitup.myself/}
 "simple_test_data"
+
+#' AC data (3x3)
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"ac_data"
+
+#' RSP data (3x3)
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"rsp_data"
+
+#' Predictions of all 3x3 theories
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"all_theories_3"
