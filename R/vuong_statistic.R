b46b8541367b9f5bdee695c808561f90c6f675a7
PhilippKuelpmann <46841729+PhilippKuelpmann@users.noreply.github.com>
2021-12-10 14:24:51 +0100

    Added a few comments for tests of inputs

--- R/vuong_statistic.R
+++ R/vuong_statistic.R
@@ -1,14 +1,18 @@
 #' Implementation of the Vuong test for our setting
 #'
-#' @param data Data of the experiment (matrix of ints/data.frame): columns are observations of action 1,2,3..., rows are treatments.
-#' @param pred_I Model prediction for a theory (matrix of doubles/data.frame): columns are prediction of playing action 1,2,3..., rows are treatments
-#' @param pred_J Model prediction for a theory (matrix of doubles/data.frame): columns are prediction of playing action 1,2,3..., rows are treatments
+#' @param data Data of the experiment (matrix of ints/data.frame): columns are observations of action 1,2,3..., rows are treatments. matrix rows: choices, columns: treatments
+#' @param pred_I Model prediction for a theory (matrix of doubles/data.frame): rows are prediction of playing action 1,2,3..., columns are treatments
+#' @param pred_J Model prediction for a theory (matrix of doubles/data.frame): rows are prediction of playing action 1,2,3..., columns are treatments
 #'
 #' @return The z score of testing theory I against theory J, given the data
 #'
 #' @examples (missing)
 #' @export
 vuong_statistic <- function(data, pred_I, pred_J) {
+  # data: all ints
+  # if colSum of predictions is 1 and each is between 0,1
+  # check if rows and columns are of the same length
+
   result <- get_llr(data, pred_I, pred_J) / get_variance_of_llr(data, pred_I, pred_J)^(.5)
   return(result)
 }
@@ -16,14 +20,17 @@ vuong_statistic <- function(data, pred_I, pred_J) {
 
 #' Getting a vuong matrix
 #'
-#' @param data Data of the experiment (matrix of ints/data.frame): columns are observations of action 1,2,3..., rows are treatments.
-#' @param theories Model predictions for all theories. (theories is a list?)
+#' @param data matrix rows: choices, columns: treatments
+#' @param theories list of matrices each in the same format as above
 #'
 #' @return Table of z-scores for all comparisons
 #'
 #' @examples (missing)
 #' @export
 vuong_matrix <- function(data, theories) {
+  # data: all ints, length(data rows) = length(theory rows), ...
+  # theories: sum of each treatment for each theory = 1
+  # in both theories and data: same number of actions (p1/p2/p3 or A/B/C) and same number of treatments (T01-T40)
   num_theories <- length(theories)
   result <- matrix(, nrow = num_theories, ncol = num_theories)
   for (i in 1:num_theories) {
