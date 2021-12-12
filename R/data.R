a62312793358fd77a2df1a9f979e0a82ab9205a6
PhilippKuelpmann <46841729+PhilippKuelpmann@users.noreply.github.com>
2021-12-12 21:50:28 +0100

    Added reformatted data

--- R/data.R
+++ R/data.R
@@ -1,3 +1,72 @@
+#' Data from the two-action games
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"data_two_action_games"
+
+#' Data from the three-action games
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"data_three_action_games"
+
+
+#' Predictions for the two-action games
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"predictions_two_action_games"
+
+#' Predictions for the three-action games
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"predictions_three_action_games"
+
+
+
+#' Data from the three-action games
+#'
+#' Blabla!
+#'
+#' @format Need to reformat anyway ...
+#' \describe{
+#'   \item{rows}{actions}
+#'   \item{columns}{treatments}
+#'   ...
+#' }
+#' @source \url{http://madeitup.myself/}
+"data_three_action_games"
+
+
 #' Simple Test Data
 #'
 #' Mostly to test data im/exporting
