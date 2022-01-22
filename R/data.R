be1fac1f6e9b1551872df80983e83d84aaa2731c
PhilippKuelpmann <philipp.kuelpmann@gmail.com>
2022-01-22 16:16:55 +0100

    removed unnecessary data

--- R/data.R
+++ R/data.R
@@ -53,24 +53,10 @@
 "predictions_three_action_games"
 
 
-
-#' Data from the three-action games
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"data_three_action_games"
-
-
 #' Simple Test Data
 #'
 #' Mostly to test data im/exporting
+#' Don't make accessible to users?
 #'
 #' @format A matrix with 3 rows and 2 columns
 #' \describe{
@@ -80,81 +66,3 @@
 #' }
 #' @source \url{http://madeitup.myself/}
 "simple_test_data"
-
-#' AC data (3x3)
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"ac_data"
-
-#' RSP data (3x3)
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"rsp_data"
-
-#' Predictions of all 3x3 theories
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"all_theories_3"
-
-#' Data: HDG data
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"hdg_data"
-
-#' Data: MP Data
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"mp_data"
-
-#' Theory predictions: 2x2 games
-#'
-#' Blabla!
-#'
-#' @format Need to reformat anyway ...
-#' \describe{
-#'   \item{rows}{actions}
-#'   \item{columns}{treatments}
-#'   ...
-#' }
-#' @source \url{http://madeitup.myself/}
-"all_theories_2"
