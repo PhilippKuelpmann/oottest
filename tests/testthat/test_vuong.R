3c04914d6f0fef87c54822c5c8c59866816d993f
PhilippKuelpmann <46841729+PhilippKuelpmann@users.noreply.github.com>
2022-03-18 18:19:55 +0100

    fixed format of the data & theories in the first test case

--- tests/testthat/test_vuong.R
+++ tests/testthat/test_vuong.R
@@ -1,6 +1,6 @@
-experimental_data <- matrix(10, nrow = 8, ncol = 2, byrow = TRUE)
-theory_1 <- matrix(c(.5, .5), nrow = 8, ncol = 2, byrow = TRUE)
-theory_2 <- matrix(c(.9, .1, .1, .9), nrow = 8, ncol = 2, byrow = TRUE)
+experimental_data <- matrix(10, nrow = 2, ncol = 8)
+theory_1 <- matrix(c(.5, .5), nrow = 2, ncol = 8)
+theory_2 <- matrix(c(.9, .1, .1, .9), nrow = 2, ncol = 8)
 
 expect_equal(
   vuong_statistic(experimental_data, theory_1, theory_2),
